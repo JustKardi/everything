f0f96fe5564426142204436695f39d0e78014007
JustKardi <kardichess@gmail.com>
2025-03-31 21:02:26 -0700

    Added all project files

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById('cardOne').addEventListener("click", () => {
+        window.location.href = 'directory/cardOne.html'
+    });
+    document.getElementById('cardTwo').addEventListener("click", () => {
+        window.location.href = 'directory/cardTwo.html'
+    });
+    document.getElementById('cardThree').addEventListener("click", () => {
+        window.location.href = 'directory/cardThree.html'
+    });
+
+});
